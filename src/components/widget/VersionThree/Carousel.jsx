95d64cb5b3614c63fa43e35e33250ceef5f942f6
Alex-La <lars6162@gmail.com>
2020-12-12 17:20:22 +0300

    Add carousel item for version three

--- src/components/widget/VersionThree/Carousel.jsx
+++ src/components/widget/VersionThree/Carousel.jsx
@@ -4,6 +4,8 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import "styles/versionThree/Carousel.css";
 
+import CarouselItem from "./CarouselItem";
+
 const Carousel = () => {
   const [width, setWidth] = useState(window.innerWidth);
 
@@ -25,157 +27,11 @@ const Carousel = () => {
       nav
       loop
     >
-      <div className="review-shop__item">
-        <div className="item-top">
-          <img src="http://marsip01.tech01.net.in/mpv/img/mo.png" alt="" />
-        </div>
-        <div className="review-open__header header-review">
-          <div className="header-review__person">
-            <img
-              className="person-img"
-              src="http://marsip01.tech01.net.in/mpv/img/person.png"
-              alt="Артемий Лебедев"
-            />
-            <div className="person-name">
-              <h5>Артемий Лебедев</h5>
-              <p>Россия, Волгоград</p>
-            </div>
-          </div>
-          <div className="header-review__rating rating-header">
-            <div className="rating-header__stars">
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-starsg.svg"
-                alt="star"
-              />
-            </div>
-            <span>3 месяца назад</span>
-          </div>
-          <p className="full-review">
-            Это мой первый компьютер семейства Mac. Поскольку сам работаю в
-            сфере ИТ, и всю жизнь работал на Windows, решил для себя попробовать
-            что-то новое, поизучать операционную систему MacOS. Поначалу было
-            непривычно, пришлось привыкать. Через какое-то время начал понимать,
-            что в MacOS работать одно удовольствие.
-          </p>
-        </div>
-      </div>
-
-      <div className="review-shop__item">
-        <div className="item-top">
-          <img src="http://marsip01.tech01.net.in/mpv/img/mo.png" alt="" />
-        </div>
-        <div className="review-open__header header-review">
-          <div className="header-review__person">
-            <img
-              className="person-img"
-              src="http://marsip01.tech01.net.in/mpv/img/person.png"
-              alt="Артемий Лебедев"
-            />
-            <div className="person-name">
-              <h5>Артемий Лебедев</h5>
-              <p>Россия, Волгоград</p>
-            </div>
-          </div>
-          <div className="header-review__rating rating-header">
-            <div className="rating-header__stars">
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-starsg.svg"
-                alt="star"
-              />
-            </div>
-            <span>3 месяца назад</span>
-          </div>
-          <p className="full-review">
-            Это мой первый компьютер семейства Mac. Поскольку сам работаю в
-            сфере ИТ, и всю жизнь работал на Windows, решил для себя попробовать
-            что-то новое, поизучать операционную систему MacOS. Поначалу было
-            непривычно, пришлось привыкать. Через какое-то время начал понимать,
-            что в MacOS работать одно удовольствие.
-          </p>
-        </div>
-      </div>
-      <div className="review-shop__item">
-        <div className="item-top">
-          <img src="http://marsip01.tech01.net.in/mpv/img/mo.png" alt="" />
-        </div>
-        <div className="review-open__header header-review">
-          <div className="header-review__person">
-            <img
-              className="person-img"
-              src="http://marsip01.tech01.net.in/mpv/img/person.png"
-              alt="Артемий Лебедев"
-            />
-            <div className="person-name">
-              <h5>Артемий Лебедев</h5>
-              <p>Россия, Волгоград</p>
-            </div>
-          </div>
-          <div className="header-review__rating rating-header">
-            <div className="rating-header__stars">
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-stars.svg"
-                alt="star"
-              />
-              <img
-                src="http://marsip01.tech01.net.in/mpv/img/rating-starsg.svg"
-                alt="star"
-              />
-            </div>
-            <span>3 месяца назад</span>
-          </div>
-          <p className="full-review">
-            Это мой первый компьютер семейства Mac. Поскольку сам работаю в
-            сфере ИТ, и всю жизнь работал на Windows, решил для себя попробовать
-            что-то новое, поизучать операционную систему MacOS. Поначалу было
-            непривычно, пришлось привыкать. Через какое-то время начал понимать,
-            что в MacOS работать одно удовольствие.
-          </p>
-        </div>
-      </div>
+      {Array(3)
+        .fill()
+        .map(() => (
+          <CarouselItem />
+        ))}
     </OwlCarousel>
   );
 };
