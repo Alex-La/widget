a36638493e77dbaca58f20041f028d108abc3cd0
Alex-La <lars6162@gmail.com>
2020-12-15 13:57:32 +0300

    Show version

--- src/components/widget/MainWindow/WindowReviews/WindowReview/ReviewOpen/Answer.jsx
+++ src/components/widget/MainWindow/WindowReviews/WindowReview/ReviewOpen/Answer.jsx
@@ -6,15 +6,24 @@ const Answer = () => {
     <div className="review_answer">
       <a href="#" className="review_answer__do">
         <span>Ответить</span>
-        <img src="http://localhost:4000/static/answers.svg" alt="Ответить" />
+        <img
+          src="http://marsip01.tech01.net.in/mpv/img/answers.svg"
+          alt="Ответить"
+        />
       </a>
       <div className="review_answer__mark">
         <a href="#" className="like">
-          <img src="http://localhost:4000/static/like-review.svg" alt="5" />
+          <img
+            src="http://marsip01.tech01.net.in/mpv/img/like-review.svg"
+            alt="5"
+          />
           <span>5</span>
         </a>
         <a href="#" className="dislike">
-          <img src="http://localhost:4000/static/dislike-review.svg" alt="0" />
+          <img
+            src="http://marsip01.tech01.net.in/mpv/img/dislike-review.svg"
+            alt="0"
+          />
           <span>0</span>
         </a>
       </div>
